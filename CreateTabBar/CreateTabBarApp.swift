d63de024d45f2a598297787fd761fa3ecf993621
Mouygech Leang <leangmouygech68@gmail.com>
2022-06-01 16:33:18 +0700

    Initial Commit

---
+++ CreateTabBar/CreateTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateTabBarApp.swift
+//  CreateTabBar
+//
+//  Created by Leng Mouyngech on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CreateTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
